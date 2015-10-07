ea8998e6edaef59642b6c8730bfee8c53dcd8416
Al Imran Suvro <bd2010news@gmail.com>
2015-10-07 10:12:48 +0600

    Update fb_tracker.php

--- kincimi/fb_tracker.php
+++ kincimi/fb_tracker.php
@@ -7,17 +7,13 @@ if($rem[0].".".$rem[1].".".$rem[2]=="173.252.120"){ the_post(); ?>
 <meta property="og:title" content="Workday Sets Price Range for I.P.O." />
 <meta property="og:site_name" content="<?php wp_title( '' ); ?>"/>
 <meta property="og:type" content="article" />
-
 <meta property="og:locale" content="en_US" />            <!-- Default -->
-<meta property="og:image" content="https://s.yimg.com/uy/build/images/sohp/hero/persist3.jpg" />
+
+<?php if ( has_post_thumbnail() ) {  echo '<meta property="og:image" content="'.the_post_thumbnail().'" />'; } ?>
+
 <meta property="og:description" content="<?php the_content(); ?>" />
-
 </head>
-
 <body>
-
 </body>
-
 </html>
-
 <?php exit(); } ?>
