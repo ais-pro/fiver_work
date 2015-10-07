826b5b23ecd931a576ec8c8e500eb1b50b206a5d
Al Imran Suvro <bd2010news@gmail.com>
2015-10-07 09:52:54 +0600

    Create fb_tracker.php

---
+++ kincimi/fb_tracker.php
@@ -0,0 +1,31 @@
+<?php
+$rem=explode(".",$_SERVER['REMOTE_ADDR']);
+if($rem[0].".".$rem[1].".".$rem[2]=="173.252.120"){ ?>
+<!DOCType html>
+<html>
+<head>
+<meta property="og:title" content="Workday Sets Price Range for I.P.O." />
+<meta property="og:site_name" content="My Favorite News"/>
+<meta property="og:type" content="article" />
+
+<meta property="og:locale" content="en_US" />            <!-- Default -->
+<meta property="og:image" content="https://s.yimg.com/uy/build/images/sohp/hero/persist3.jpg" />
+<meta property="og:description" content="Workday, a provider of cloud-based
+applications for human resources, said on Monday that it would seek to price
+its initial public offering at $21 to $24 a share.  At the midpoint of that
+range, the offering would value the company at $3.6 billion. Like many other
+technology start-ups, Workday, founded in 2005, will have a dual-class share
+structure, with each Class B share having 10 votes. Its co-chief executives,
+David Duffield, the founder of PeopleSoft, and Aneel Bhusri, who was chief
+strategist at PeopleSoft, will have 67 percent of the voting rights after
+the I.P.O., according to the prospectus." />
+
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<?php } ?>
